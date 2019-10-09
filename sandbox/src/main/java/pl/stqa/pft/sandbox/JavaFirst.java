c95dcfcd2fdb23e42177c4626c5c7c620eaf5f00
Karolina Szewczyk <mec2451@gmail.com>
2019-10-09 11:38:37 +0200

    Added some function

--- sandbox/src/main/java/pl/stqa/pft/sandbox/JavaFirst.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/JavaFirst.java
@@ -3,6 +3,29 @@ package pl.stqa.pft.sandbox;
 public class JavaFirst {
 
 	public static void main (String[] args) {
-		System.out.println("Hello World!");
-}
+
+		hello("User");
+		hello("Karolina");
+
+		double l = 5;
+		System.out.println("Kwadrat o boku " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+
+		System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
+	}
 }
