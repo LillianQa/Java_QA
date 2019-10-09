a137e902610f2d6c494148b487044ac721f59189
Karolina Szewczyk <mec2451@gmail.com>
2019-10-09 12:23:04 +0200

    Square & Rectangle class

--- sandbox/src/main/java/pl/stqa/pft/sandbox/JavaFirst.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/JavaFirst.java
@@ -7,13 +7,11 @@ public class JavaFirst {
 		hello("User");
 		hello("Karolina");
 
-		double l = 5;
-		System.out.println("Kwadrat o boku " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Kwadrat o boku " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-
-		System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
 
 	}
 
@@ -21,11 +19,4 @@ public class JavaFirst {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area (double len) {
-		return len * len;
-	}
-
-	public static double area(double a, double b) {
-		return a * b;
-	}
 }
