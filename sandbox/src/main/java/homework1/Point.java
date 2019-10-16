5ef52903af5fb82432b998cf8a42ff6af89e1a26
Karolina Szewczyk <mec2451@gmail.com>
2019-10-16 19:43:05 +0200

    Homework 1 ver.2

--- sandbox/src/main/java/homework1/Point.java
+++ sandbox/src/main/java/homework1/Point.java
@@ -10,12 +10,16 @@ public class Point {
     this.y = y;
   }
 
-  public double distance(Point p1, Point p2) {
+  public double distance(Point p) {
+    return this.getX() - p.getX() + this.getY() - p.getY();
+  }
 
-    double dx = (p2.x - p1.x);
-    double dy = (p2.y - p1.y);
-    return Math.sqrt((dx*dx) * 2 + ((dy*dy)) * 2);
+  public double getX() {
+    return x;
+  }
 
+  public double getY() {
+    return y;
   }
 
   public static void main (String[] args) {
@@ -23,38 +27,6 @@ public class Point {
     Point p1 = new Point(5.6, 7.3);
     Point p2 = new Point(8.6, 2.5);
 
-//    System.out.println("Odlegosc od " + p1.x + " i " +  p1.y + " oraz " +  p2.x  + " i " + p2.y  + " wynosi " + distance(p1, p2));
-  }
+    System.out.println("Odlegosc od " + p1.x + " i " +  p1.y + " oraz " +  p2.x  + " i " + p2.y  + " wynosi " + p1.distance(p2) );
+  }}
 
-}
-
-//class FooBuilder {
-//  private String a = "";
-//  private Integer b = 0;
-//
-//  FooBuilder setA(String a) {
-//    this.a = a;
-//    return this;
-//  }
-//
-//  FooBuilder setB(Integer b) {
-//    this.b = b;
-//    return this;
-//  }
-//
-//  public class Rectangle {
-//
-//    public double a;
-//    public double b;
-//
-//    public Rectangle(double a, double b) {
-//
-//      this.a = a;
-//      this.b = b;
-//    }
-//
-//    public double area() {
-//      return this.a * this.b;
-//    }
-//
-//  }}
