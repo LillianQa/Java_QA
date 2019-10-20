0bd1ae9ea399274e7b968a41d5658a075b29db55
Karolina Szewczyk <mec2451@gmail.com>
2019-10-20 16:17:39 +0200

    Test Base contains data from newContactCreation

--- addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/TestBase.java
@@ -58,17 +58,42 @@ public class TestBase {
     wd.findElement(By.linkText("groups")).click();
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-
-  }
-
   protected void deleteSelectedGroup() {
     wd.findElement(By.name("delete")).click();
   }
 
   protected void selectGroup() {
     wd.findElement(By.name("selected[]")).click();
+  }
+
+  protected void submitNewContactForm() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  protected void fillNewContactForm(newContactData newContactData) {
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(newContactData.getFirstname());
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(newContactData.getLastname());
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(newContactData.getTitle());
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(newContactData.getCompany());
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(newContactData.getHome());
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(newContactData.getMobilenumber());
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(newContactData.getEmail());
+  }
+
+  protected void gotoAddNewContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown()  {
+    wd.quit();
+
   }
 }
