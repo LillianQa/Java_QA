5339d7e43ba134e1ea214c934edeafc611045d2f
Karolina Szewczyk <mec2451@gmail.com>
2019-10-29 13:21:56 +0100

    Optimalization for for the transition between page

--- addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appManager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appManager/NavigationHelper.java
@@ -1,15 +1,28 @@
 package pl.stqa.pft.adressbook.appManager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
-  public NavigationHelper(FirefoxDriver wd) {
+  public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
-  public  void gotoGroupPage() {
-    click(By.linkText("groups"));
+  public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
+      click(By.linkText("groups"));
+    }
+  
+
+  public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
+    click(By.linkText("home"));
   }
 }
