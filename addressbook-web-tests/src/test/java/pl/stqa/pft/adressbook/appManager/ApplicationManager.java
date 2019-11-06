f192dfd3ff1cdb52d21d9dca76f0c9dac543f3ee
Karolina Szewczyk <mec2451@gmail.com>
2019-11-06 15:26:27 +0100

    Changes in address of site

--- addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appManager/ApplicationManager.java
@@ -35,7 +35,7 @@ public class ApplicationManager {
         wd = new SafariDriver();
       }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/index.php");
+    wd.get("http://localhost/addressbook");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     contactHelper = new ContactHelper(wd);
