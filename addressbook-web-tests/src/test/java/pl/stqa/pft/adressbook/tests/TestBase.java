3b2d3d55040718dfacadddea80b15da882678df9
Karolina Szewczyk <mec2451@gmail.com>
2019-12-02 13:52:36 +0100

    TestBase updated logger

--- addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/tests/TestBase.java
@@ -1,13 +1,22 @@
 package pl.stqa.pft.adressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.stqa.pft.adressbook.appManager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase  {
 
-  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser"), BrowserType.CHROME);
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
@@ -17,6 +26,17 @@ public class TestBase  {
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception{
     app.stop();
+
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test" + m.getName() + "with parameters" + Arrays.asList(p)); // we need to convert output as a list, because there's no string
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m, Object[] p) {
+    logger.info("Start stop" + m.getName());
 
   }
 
