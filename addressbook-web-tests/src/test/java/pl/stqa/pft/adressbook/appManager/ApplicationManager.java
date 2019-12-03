275cf750293e82adef25563f7cc4992d97847f5e
Karolina Szewczyk <mec2451@gmail.com>
2019-12-03 20:39:14 +0100

    AM updated DB Helper

--- addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appManager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
   private String browser;
+  private DBHElper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -33,6 +34,7 @@ public class ApplicationManager {
   public  void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+    dbHelper = new DBHElper();
       if (browser.equals(BrowserType.FIREFOX)) {
         wd = new FirefoxDriver();
       } else if (browser.equals(BrowserType.CHROME)) {
@@ -47,6 +49,7 @@ public class ApplicationManager {
     contactHelper = new ContactHelper(wd);
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+
   }
 
   public  void stop() {
@@ -64,4 +67,6 @@ public class ApplicationManager {
   public ContactHelper contact() {
     return contactHelper;
   }
+
+  public DBHElper db() {return dbHelper;}
 }
