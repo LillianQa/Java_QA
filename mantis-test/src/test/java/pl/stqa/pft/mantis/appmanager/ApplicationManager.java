7c8588d9cd3dadc6102dbe260ccbe62b0091374a
Karolina Szewczyk <mec2451@gmail.com>
2019-12-05 15:55:28 +0100

    App update

--- mantis-test/src/test/java/pl/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-test/src/test/java/pl/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -15,9 +15,10 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
 
 
   public ApplicationManager(String browser) {
@@ -28,6 +29,32 @@ public class ApplicationManager {
   public  void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+  }
+
+  public  void stop() {
+    if (wd != null) {
+      wd.quit();
+    }
+  }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+
+  }
+  public WebDriver getDriver() {
+    if (wd == null) {
       if (browser.equals(BrowserType.FIREFOX)) {
         wd = new FirefoxDriver();
       } else if (browser.equals(BrowserType.CHROME)) {
@@ -35,12 +62,9 @@ public class ApplicationManager {
       } else if (browser.equals(BrowserType.SAFARI)) {
         wd = new SafariDriver();
       }
-    wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-
-  }
-
-  public  void stop() {
-    wd.quit();
+      wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
